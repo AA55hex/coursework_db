f8161dfbae893faaa88e442e38080977f5fe0ee5
AA55hex <69757386+AA55hex@users.noreply.github.com>
2020-12-27 16:18:18 +0300

    Update BusinessLogic.sql

--- BusinessLogic.sql
+++ BusinessLogic.sql
@@ -1,6 +1,35 @@
 ﻿use MyKindergarden;
 GO
-
+create view Only_visit_dates as
+	(select * from VisitDate where IsVisitDate != 0)
+GO
+create function Is_workday(@d date)
+	returns bit
+	as
+	begin
+	if(DATEPART(dw, @d) < 6)
+		return 1;
+	return 0;
+	end;
+GO
+create procedure Insert_date
+	@d date
+as
+	if @d not in (select VisitDate.Date from VisitDate)
+		insert VisitDate(Date, IsVisitDate) values (@d, dbo.Is_workday(@d));
+GO
+create procedure Insert_dates 
+	@d1 date,
+	@d2 date
+as
+	declare @buff date;
+	set @buff = @d1;
+	while @buff <= @d2
+	begin
+		execute Insert_date @buff;
+		SET @buff=DATEADD(DAY,1,@buff);
+	end;
+GO
 create function MinDate (@d1 date, @d2 date) 
 	returns date
 	as
@@ -34,18 +63,33 @@ AS
 		END;
 GO
 
-create trigger VisitDateUpdateBlock on VisitDate instead of UPDATE
+create trigger VisitDateUpdateBlock on VisitDate after UPDATE
 as
-	RAISERROR ('VisitDate can`t be updated', 16, 1);  
-	ROLLBACK TRANSACTION;  
+	if exists(select inserted.* from inserted join deleted on deleted.Id = inserted.Id and deleted.Date != inserted.Date)
+		RAISERROR ('Date can`t be updated', 16, 1);  
+		ROLLBACK TRANSACTION;  
 	RETURN
+GO
+create trigger VisitDateUpdate on VisitDate after UPDATE
+as
+	delete from VisitNote 
+		where VisitNote.Date in
+		(select inserted.Id from inserted where inserted.IsVisitDate = 0);
+	insert into VisitNote(Child, Date, Visited)
+		select chld.Id, inserted.Id, 0 
+		from (select inserted.* from inserted join deleted 
+				on deleted.Id = inserted.Id 
+				and inserted.IsVisitDate = 1 
+				and deleted.IsVisitDate = 0) as inserted
+		cross join Child chld  
+			where inserted.Date between chld.VisitStart and chld.VisitEnd;
 GO
 create trigger VisitDateInsert on VisitDate after INSERT
 as
-	insert into VisitNote(Child,Date)
-		select chld.Id, inserted.Id from inserted
-		cross join (select * from Child) chld  
-			where inserted.Date between chld.VisitStart and dbo.MinDate(GETDATE(), chld.VisitEnd);
+	insert into VisitNote(Child, Date, Visited)
+		select chld.Id, inserted.Id, 0 from (select * from inserted where inserted.IsVisitDate = 1) as inserted
+		cross join Child chld  
+			where inserted.Date between chld.VisitStart and chld.VisitEnd;
 GO
 
 
@@ -69,29 +113,32 @@ AS
 	END;
 GO
 
-create trigger VisitNoteDateUpdate ON Child after UPDATE 
+create trigger VisitNoteDateDelete ON Child after UPDATE 
 AS
 	delete from VisitNote where Id = 
 		(
 			select VisitNote.Id from VisitNote 
 				join VisitDate on VisitDate.Id = VisitNote.Date
 				join inserted on inserted.Id = VisitNote.Child 
-				and VisitDate.Date not between inserted.VisitStart and dbo.MinDate(GETDATE(), inserted.VisitEnd)
+				and VisitDate.Date not between inserted.VisitStart and inserted.VisitEnd
 		);	
 GO
 create trigger UpdateVisitNoteAddition ON Child after UPDATE 
 AS
-	insert into VisitNote(Child, Date)
-		select inserted.Id, vdate.Id from inserted
-		cross join VisitDate as vdate 
-			where vdate.Date between inserted.VisitStart and dbo.MinDate(GETDATE(), inserted.VisitEnd)
+	insert into VisitNote(Child, Date, Visited)
+		select inserted.Id, vdate.Id, 0 from 
+			(select inserted.* from inserted join deleted 
+				on deleted.Id = inserted.Id
+				and (deleted.VisitStart != inserted.VisitStart or deleted.VisitEnd != inserted.VisitEnd)) inserted
+		cross join Only_visit_dates as vdate 
+			where vdate.Date between inserted.VisitStart and inserted.VisitEnd
 			and not exists(select * from VisitNote where VisitNote.Date = vdate.Id and VisitNote.Child = inserted.Id);
 GO
 create trigger InsertVisitNoteAddition ON Child after INSERT
 AS
-	insert into VisitNote(Child, Date)
+	insert into VisitNote(Child, Date, Visited)
 		select inserted.Id, vdate.Id from inserted
-		cross join VisitDate as vdate 
-			where vdate.Date between inserted.VisitStart and dbo.MinDate(GETDATE(), inserted.VisitEnd);
+		cross join Only_visit_dates as vdate 
+			where vdate.Date between inserted.VisitStart and inserted.VisitEnd;
 GO
 
