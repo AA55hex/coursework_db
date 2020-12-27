b71324abf630e4b06a929950c4febbf5f98d0a3c
AA55hex <69757386+AA55hex@users.noreply.github.com>
2020-12-27 21:05:30 +0300

    Update BusinessLogic.sql

--- BusinessLogic.sql
+++ BusinessLogic.sql
@@ -30,18 +30,21 @@ as
 		SET @buff=DATEADD(DAY,1,@buff);
 	end;
 GO
-create function MinDate (@d1 date, @d2 date) 
+create function LastVisitDate()
 	returns date
 	as
 	begin
-	return case 
-		when @d1 = null then @d2
-		when @d2 = null then @d1
-		when @d1 > @d2 then @d2
-		else @d1
-		end
+		return (select top 1 Date from VisitDate order by Date desc)
 	end;
-
+GO
+create function SelectDate (@d date, @alt date) 
+	returns date
+	as
+	begin
+	if(@d = null)
+		return @alt;
+	return @d;
+	end;
 GO
 
 create trigger ClientUsrPwdConstraint ON Client after INSERT, UPDATE 
@@ -82,14 +85,14 @@ as
 				and inserted.IsVisitDate = 1 
 				and deleted.IsVisitDate = 0) as inserted
 		cross join Child chld  
-			where inserted.Date between chld.VisitStart and chld.VisitEnd;
+			where inserted.Date between chld.VisitStart and dbo.SelectDate(chld.VisitEnd, dbo.LastVisitDate());
 GO
 create trigger VisitDateInsert on VisitDate after INSERT
 as
 	insert into VisitNote(Child, Date, Visited)
 		select chld.Id, inserted.Id, 0 from (select * from inserted where inserted.IsVisitDate = 1) as inserted
 		cross join Child chld  
-			where inserted.Date between chld.VisitStart and chld.VisitEnd;
+			where inserted.Date between chld.VisitStart and dbo.SelectDate(chld.VisitEnd, dbo.LastVisitDate());
 GO
 
 
@@ -113,6 +116,15 @@ AS
 	END;
 GO
 
+create trigger WrongDatesBlock ON Child after INSERT, Update
+AS
+	if exists(select * from inserted where inserted.VisitStart > inserted.VisitEnd)
+	BEGIN  
+		RAISERROR ('Wrong dates.', 16, 1);  
+		ROLLBACK TRANSACTION;  
+		RETURN   
+	END;
+GO
 create trigger VisitNoteDateDelete ON Child after UPDATE 
 AS
 	delete from VisitNote where Id = 
@@ -120,7 +132,7 @@ AS
 			select VisitNote.Id from VisitNote 
 				join VisitDate on VisitDate.Id = VisitNote.Date
 				join inserted on inserted.Id = VisitNote.Child 
-				and VisitDate.Date not between inserted.VisitStart and inserted.VisitEnd
+				and VisitDate.Date not between inserted.VisitStart and dbo.SelectDate(inserted.VisitEnd, dbo.LastVisitDate())
 		);	
 GO
 create trigger UpdateVisitNoteAddition ON Child after UPDATE 
@@ -131,14 +143,15 @@ AS
 				on deleted.Id = inserted.Id
 				and (deleted.VisitStart != inserted.VisitStart or deleted.VisitEnd != inserted.VisitEnd)) inserted
 		cross join Only_visit_dates as vdate 
-			where vdate.Date between inserted.VisitStart and inserted.VisitEnd
+			where vdate.Date between inserted.VisitStart and dbo.SelectDate(inserted.VisitEnd, dbo.LastVisitDate())
 			and not exists(select * from VisitNote where VisitNote.Date = vdate.Id and VisitNote.Child = inserted.Id);
 GO
 create trigger InsertVisitNoteAddition ON Child after INSERT
 AS
 	insert into VisitNote(Child, Date, Visited)
-		select inserted.Id, vdate.Id from inserted
+		select inserted.Id, vdate.Id, 0 from inserted
 		cross join Only_visit_dates as vdate 
-			where vdate.Date between inserted.VisitStart and inserted.VisitEnd;
+			where vdate.Date between inserted.VisitStart and dbo.SelectDate(inserted.VisitEnd, dbo.LastVisitDate());
 GO
+
 
